4b755c63cc9194fac8cfb964c28c7204bf3a972e
reactive <reactivegame@hotmail.com>
2019-06-23 23:14:29 +0300

    init

---
+++ app/src/main/java/com/example/twitter/MainLogin.kt
@@ -0,0 +1,12 @@
+package com.example.twitter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
